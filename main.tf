9d4ef559a86333b0f2c97ea6629224074b3ff004
Robert Peteuil <robert@peteuil.com>
2019-04-16 09:15:52 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 0.11.1"
+}
+
+provider "google" {
+  credentials = "${var.gcp_credentials}"
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+data "google_compute_image" "latest" {
+  family  = "${var.image_family}"
+  project = "${var.image_project}"
+}
+
+resource "google_compute_instance" "demo" {
+  name         = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.gcp_zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${data.google_compute_image.latest.name}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "gcp_credentials" {
+  description = "GCP credentials needed by google provider"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default     = "us-east1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-a"
+  default     = "us-east1-b"
+}
+
+variable "machine_type" {
+  description = "GCP machine type"
+  default     = "n1-standard-1"
+}
+
+variable "instance_name" {
+  description = "GCP instance name"
+  default     = "demo"
+}
+
+variable "image" {
+  description = "image to build instance from"
+  default     = "debian-cloud/debian-9"
+}
+
+variable "image_family" {
+  description = "image family to build instance from"
+  default     = "ubuntu-1804-lts"
+}
+
+variable "image_project" {
+  description = "image project to build instance from"
+  default     = "ubuntu-os-cloud"
+}
